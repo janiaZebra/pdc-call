95f5727c6b4bfd538f68f5c42a9ea73f6aed359f
janiaZebra <julian.ania@zebraventures.eu>
2025-07-09 13:00:57 +0200

    Remove unnecessary blank line in OpenAI WebSocket connection code

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ async def ws_audio(websocket: WebSocket):
 
         openai_ws = await websockets.connect(OPENAI_WS_URL, additional_headers=headers )
         logger.info("Connected to OpenAI WebSocket")
-
         session_config = {
             "type": "session.update",
             "session": {
